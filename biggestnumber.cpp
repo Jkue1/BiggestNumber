af5358869ed10b85725e0268c6feab68ae1e9442
jkue1 <jkue1@mail.csuchico.edu>
2018-02-06 15:35:01 -0800

    add variables

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -5,13 +5,24 @@ using namespace std; //so we don't need the prefix std::
 
 int main()
 {
+int whole; 
+int swhole;
+int biggest;
   
-  cout<<"Please enter a whole number:\n";
   
-  cout<<"Please enter another whole number:\n";
+  cout <<"Please enter a whole number:\n"; 
+  std::cin >> whole;
+  
+  cout <<"Please enter another whole number:\n";
+  std::cin >> swhole;
 
-  cout<<"Of those two numbers, the biggest is: ";
-  cout<<endl<<"Thank you for playing.\n";
+  if (whole > swhole)
+    {biggest = whole;}
+
+  else {biggest = swhole;}
+  cout <<"Of those two numbers, the biggest is:" <<  biggest << std::endl;
+  
+  cout << endl <<"Thank you for playing.\n";
 
   return 0;
 }
